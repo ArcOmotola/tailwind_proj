4b6504c95885da651fc372180e049775d08b9cbe
Omotola Ogunsola <ogunsola40@gmail.com>
2022-04-24 21:05:36 +0100

    added resort destination images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Destinations from "./components/Destinations";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Destinations />
     </div>
   );
 }
